3f875dbc3587e870f7be6b777f10c06dbea93e94
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-18 08:10:21 +0000

    Automatically backed up by Learn

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -1,5 +1,6 @@
 katz_deli = ["Ada", "Grace", "Kent"]
 counter = 1
 katz_deli.each do|ticket|
-puts "Welcome #{katz_deli}. You are number #{counter}"
-puts "Now serving #{katz_deli}"
+  puts "Welcome #{katz_deli}. You are number #{counter}"
+  puts "Now serving #{katz_deli}"
+counter +=
